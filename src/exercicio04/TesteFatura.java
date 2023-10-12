c9e8787d4c1f45799294b8efcbe1670ceac16d38
Fernanda Coutinho <fernandarcoutinho@gmail.com>
2023-10-12 19:51:51 -0300

    Teste Exercicio4

--- src/exercicio04/TesteFatura.java
+++ src/exercicio04/TesteFatura.java
@@ -2,8 +2,8 @@ package exercicio04;
 
 public class TesteFatura {
     public static void main(String[] args){
-        Fatura fatura = new Fatura("123", "Venda de 5 itens",
-                5, 21.0);
+        Fatura fatura = new Fatura("123", "Venda de 3 itens",
+                3, 36.0);
 
         System.out.println(fatura.getValorFatura());
     }
